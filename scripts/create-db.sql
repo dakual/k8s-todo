7f62a4ae3b2c159625e8a5599fa19dd1e566b8e4
Daghan Kurtay Altunsoy <daghan.altunsoy@gmail.com>
2023-04-02 21:10:24 +0300

    first commit

--- scripts/create-db.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER IF NOT EXISTS 'user'@'localhost' IDENTIFIED BY 'password';
-CREATE DATABASE IF NOT EXISTS `stg` DEFAULT CHARACTER SET `utf8` COLLATE `utf8_unicode_ci`;
-GRANT ALL ON `stg`.* TO 'user'@'localhost';
-USE `stg`;
